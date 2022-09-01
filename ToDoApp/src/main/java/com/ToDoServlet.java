b680809d6f65442a48facf77b724f75c39758a91
nivanjanapramod <nivanjanapramod23@gmail.com>
2022-09-01 11:30:12 +0530

    first commit

---
+++ ToDoApp/src/main/java/com/ToDoServlet.java
@@ -0,0 +1,202 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.struts2.ServletActionContext;
+import org.apache.struts2.interceptor.ServletRequestAware;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.opensymphony.xwork2.ActionSupport;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import com.todo.apps.Todo;
+
+
+public class ToDoServlet extends ActionSupport{
+	private static final long serialVersionUID = 1L;
+	private HttpServletRequest request;
+	private HttpServletResponse response;
+	
+
+	public String execute() throws ServletException, IOException, JSONException {
+		request = ServletActionContext.getRequest();
+		response = ServletActionContext.getResponse();
+		String action = request.getServletPath();
+		switch(action) {
+		case "/task/insert" : doPut(request,response);
+						break;
+		case "/task/update" : doPost(request,response);
+						break;
+		case "/task/delete" : doDelete(request,response);
+						break;
+		case "/task/get" : doGet(request,response);
+					break;
+		case "/task/getEditForm" : selectTodoById(request, response);
+									break;
+		}
+		return null;
+	}
+	
+    public ToDoServlet() {
+        super();    
+    }
+
+	
+	public static Connection getConnection() {
+		Connection connection = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test?useSSL=false","root","Niva@2000");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return connection;
+	}
+	
+	private void printSQLException(SQLException ex) {
+		for (Throwable e : ex) {
+			if (e instanceof SQLException) {
+				e.printStackTrace(System.err);
+				System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+				System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+				System.err.println("Message: " + e.getMessage());
+				Throwable t = ex.getCause();
+				while (t != null) {
+					System.out.println("Cause: " + t);
+					t = t.getCause();
+				}
+			}
+		}
+	}
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, JSONException {
+		try(
+			Connection connection = getConnection()){
+			PreparedStatement preparedStatement = connection.prepareStatement("select * from todo;");
+			System.out.println(preparedStatement);
+			ResultSet rs = preparedStatement.executeQuery();
+			JSONArray ja = new JSONArray();
+			while (rs.next()) {
+				JSONObject j = new JSONObject();
+				j.put("id", rs.getInt("id"));
+				j.put("title", rs.getString("title"));
+				j.put("description", rs.getString("description"));
+				j.put("completed", rs.getBoolean("completed"));
+				ja.put(j);
+			}
+			request.setAttribute("list", ja);
+			System.out.println(ja);
+			response.setContentType("application/json");  
+			PrintWriter out = response.getWriter();
+			out.print(ja);
+			out.flush();
+		}
+		catch(SQLException e) {
+			printSQLException(e);
+		}
+	}
+	
+	protected void selectTodoById(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, JSONException {
+		try(
+			Connection connection = getConnection()){
+			PreparedStatement preparedStatement = connection.prepareStatement("select * from todo where id=?;");
+			int id = Integer.valueOf(request.getParameter("id"));
+			preparedStatement.setInt(1, id);
+			ResultSet rs = preparedStatement.executeQuery();
+			JSONObject j = new JSONObject();
+			while (rs.next()) {
+				j.put("id", rs.getInt("id"));
+				j.put("title", rs.getString("title"));
+				j.put("description", rs.getString("description"));
+				j.put("completed", rs.getInt("completed"));
+			}
+			response.setContentType("application/json");  
+			PrintWriter out = response.getWriter();
+			out.print(j);
+			out.flush();
+		}
+		catch(SQLException e) {
+			printSQLException(e);
+		}
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try(Connection connection = getConnection()){
+			PreparedStatement preparedStatement = connection.prepareStatement("update todo set title = ?,description= ?, completed =? where id = ?;");
+			String description = (String) request.getParameter("description");
+			String title = (String) request.getParameter("title");
+			int id = Integer.valueOf(request.getParameter("id"));
+			int completed =Integer.valueOf(request.getParameter("completed"));
+			Todo todo = new Todo(id, title, description, completed);
+			preparedStatement.setInt(4, id);
+			preparedStatement.setString(1, todo.getTitle());
+			preparedStatement.setString(2, todo.getDescription());
+			preparedStatement.setInt(3, todo.isCompleted());
+			preparedStatement.executeUpdate();
+			connection.commit();
+			preparedStatement.close();
+			connection.close();
+
+		}
+		catch(SQLException e) {
+			printSQLException(e);
+		}
+		//doGet(request, response);
+	}
+
+	
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try(Connection connection = getConnection()){
+			String description = (String) request.getParameter("description");
+			String title = (String) request.getParameter("title");
+			int completed =Integer.valueOf(request.getParameter("completed"));
+			Todo todo = new Todo(title, description, completed);
+			PreparedStatement preparedStatement = connection.prepareStatement("insert into todo (title,description,completed) values(?,?,?);");
+			System.out.println(preparedStatement);
+			//ResultSet rs = preparedStatement.executeQuery();
+			preparedStatement.setString(1, todo.getTitle());
+			preparedStatement.setString(2, todo.getDescription());
+			preparedStatement.setInt(3, todo.isCompleted());
+			preparedStatement.executeUpdate();
+			connection.commit();
+			preparedStatement.close();
+			connection.close();
+		}
+		catch(SQLException e) {
+			printSQLException(e);
+		}
+	}
+
+	
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try(Connection connection = getConnection()){
+			PreparedStatement preparedStatement = connection.prepareStatement("delete from todo where id=?");
+			int id = Integer.valueOf(request.getParameter("id"));
+			preparedStatement.setInt(1, id);
+			System.out.println(preparedStatement);
+			preparedStatement.executeUpdate();
+			//ResultSet rs = preparedStatement.executeQuery();
+			preparedStatement.setInt(1, request.getContentLength());
+		}
+		catch(SQLException e) {
+			printSQLException(e);
+		}
+	}
+
+}
---
+++ ToDoApp/src/main/java/com/todo/apps/Todo.java
@@ -0,0 +1,49 @@
+package com.todo.apps;
+
+public class Todo {
+	public Todo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Todo(String title, String description, int completed) {
+		super();
+		this.title = title;
+		this.description = description;
+		this.completed = completed;
+	}
+	public Todo(int id, String title, String description, int completed) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.completed = completed;
+	}
+	private int id;
+	private String title;
+	private String description;
+	private int completed;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int isCompleted() {
+		return completed;
+	}
+	public void setCompleted(int completed) {
+		this.completed = completed;
+	}
+}
